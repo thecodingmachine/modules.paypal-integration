cfe1cfa11011fb3d61e3489eee010a4149670479
nguyenk <k.nguyen@thecodingmachine.com>
2012-12-10 08:56:35 +0100

    Initial Commit

---
+++ src/direct/ipn.php
@@ -0,0 +1,16 @@
+<?php
+require_once '../../../../../Mouf.php';
+
+use Mouf\MoufManager;
+
+try {
+	$instanceName = get('instance_name');
+
+	/* @var $paypalService PaypalService */
+	$paypalService = MoufManager::getMoufManager()->getInstance($instanceName);
+	$paypalService->ipn();
+} catch (\Exception $e) {
+	error_log("An error occured in the Paypal IPN: ".$e->getMessage());
+	throw $e;
+}
+?>
